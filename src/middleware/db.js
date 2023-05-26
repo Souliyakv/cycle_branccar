55a5a97f76bcd1d77ff3d368cdee4ed107bc07c2
Souliyakv <souliyaoppo21@gmail.com>
2023-05-26 20:05:00 +0700

    success

--- src/middleware/db.js
+++ src/middleware/db.js
@@ -1,7 +1,13 @@
 import mysql from 'mysql'
 import dotenv from 'dotenv'
 dotenv.config();
-const connection = mysql.createConnection(process.env.DATABASE_URL)
+const connection = mysql.createConnection({
+    host:'aws.connect.psdb.cloud',
+    user:'mkv258ac4evpa2ebinwz',
+    password:'pscale_pw_DOPkgQOwOAZ0QR3zkNEhEX98mkfMNg83DSbWTGAXkuq',
+    database:'souliya_test',
+    ssl:{"rejectUnauthorized":true}
+})
 connection.connect(function(err){
     if(err) {
         return console.log(err);
